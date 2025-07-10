92451821dc55e950ad9547aeb0655c612e55a729
Keith Maxwell <keith.maxwell@gmail.com>
2025-07-10 22:34:40 +0100

    chore: vendor noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,39 +1,35 @@
+#!/usr/bin/env -S uv run
 """Reusable nox sessions for a single file project."""
+# /// script
+# dependencies = ["nox"]
+# requires-python = ">=3.13"
+# ///
 
 from pathlib import Path
 from shutil import rmtree
-from typing import cast
 
 import nox
+from nox.sessions import Session
 
-PRIMARY = "3.13"
-VIRTUAL_ENVIRONMENT = ".venv"
-_CWD = Path().absolute()
-_BIN = _CWD / VIRTUAL_ENVIRONMENT / "bin"
-PYTHON = _BIN / "python"
+nox.options.default_venv_backend = "uv"
+
+VENV = ".venv"
+PYTHON = Path().absolute() / VENV / "bin" / "python"
 DIST = Path("dist")
 
 
-@nox.session(python=False)
-def dev(session: nox.Session) -> None:
+@nox.session()
+def dev(session: Session) -> None:
     """Set up a development environment (virtual environment)."""
-    rmtree(VIRTUAL_ENVIRONMENT, ignore_errors=True)
-    session.run(f"python{PRIMARY}", "-m", "venv", "--upgrade-deps", VIRTUAL_ENVIRONMENT)
-    session.run(PYTHON, "-m", "pip", "install", "--editable", ".[test]")
+    session.install("uv")
+    metadata = nox.project.load_toml("pyproject.toml")
+    session.run("uv", "venv", "--python", metadata["project"]["requires-python"], VENV)
+    env = {"VIRTUAL_ENV": VENV}
+    session.run("uv", "pip", "install", "--editable", ".[test]", env=env)
 
 
-@nox.session(python=PRIMARY, venv_backend="none")
-def github_output(session: nox.Session) -> None:
-    """Display outputs for CI integration."""
-    scripts = set(Path("src").glob("*.py")) - set(Path("src").glob("*_test.py"))
-    if len(scripts) > 1:
-        session.error("More than one script found in src/")
-    version = session.run(PYTHON, scripts.pop(), "--version", silent=True)
-    print("version=" + cast("str", version).strip())  # version= adds quotes
-
-
-@nox.session(python=PRIMARY)
-def distributions(session: nox.Session) -> None:
+@nox.session()
+def distributions(session: Session) -> None:
     """Produce a source and binary distribution."""
     rmtree(DIST, ignore_errors=True)
     session.install("reproducibly")
@@ -42,29 +38,16 @@ def distributions(session: nox.Session) -> None:
     session.run("reproducibly", sdist, DIST)
 
 
-@nox.session(python=PRIMARY, venv_backend="none")
-def check(session: nox.Session) -> None:
+@nox.session()
+def check(session: Session) -> None:
     """Check the built distributions with twine."""
-    session.run(_BIN / "twine", "check", "--strict", *DIST.glob("*.*"))
+    session.install("twine")
+    session.run("twine", "check", "--strict", *DIST.glob("*.*"))
 
 
-@nox.session(python=PRIMARY, venv_backend="none")
-def static(session: nox.Session) -> None:
-    """Run static analysis: usort, black and flake8.
-
-    Use the tools that were previously installed into .venv so that:
-
-    (1) the implementation or library stubs are available to type checkers
-    (2) no time is spent installing a second time
-    (3) versioning can be handled once
-
-    """
-    session.run(_BIN / "reuse", "lint")
-    session.run(_BIN / "usort", "check", "src", "noxfile.py")
-    session.run(_BIN / "black", "--check", ".")
-    session.run(_BIN / "ruff", "check", ".")
-    session.run(_BIN / "codespell")
-    session.run(_BIN / "mypy", ".")
+@nox.session(venv_backend="none", requires=["dev"])
+def static(session: Session) -> None:
+    """Run static analysis tools."""
     session.run(
         "npm",
         "exec",
@@ -72,16 +55,32 @@ def static(session: nox.Session) -> None:
         "--yes",
         "--",
         f"--pythonpath={PYTHON}",
+        external=True,
     )
 
+    def run(cmd: str) -> None:
+        session.run(PYTHON, "-m", *cmd.split(), external=True)
 
-@nox.session(python=PRIMARY, venv_backend="none")
-def test(session: nox.Session) -> None:
+    run("reuse lint")
+    run("usort check src noxfile.py")
+    run("black --check .")
+    run("ruff check .")
+    run("codespell_lib")
+    run("mypy .")
+    run("yamllint --strict .github")
+
+
+@nox.session(venv_backend="none", requires=["dev"])
+def test(session: Session) -> None:
     """Run test suite."""
     if sum(1 for _ in Path("src").glob("*_test.py")):
         session.run(PYTHON, "-m", "pytest", external=True)
     else:
         session.skip("No test files in repository")
+
+
+if __name__ == "__main__":
+    nox.main()
 
 
 # noxfile.py / https://github.com/maxwell-k/dotlocalslashbin/blob/main/noxfile.py
